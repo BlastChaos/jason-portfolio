c78a1e7ece2d03ce3a7309bfa190530ed600ff48
BlastChaos <76956740+BlastChaos@users.noreply.github.com>
2025-09-26 10:22:26 -0400

    menu instead of header

--- src/Section/Menu/ui/Menu.tsx
+++ src/Section/Menu/ui/Menu.tsx
@@ -7,24 +7,24 @@ import { TextIconButton } from "@/components/ui/button";
 type Props = {
   lang: Locale;
 };
-export const Header: React.FC<Props> = async (props: Props) => {
-  const { header } = await getDictionary(props.lang);
+export const Menu: React.FC<Props> = async (props: Props) => {
+  const { menu } = await getDictionary(props.lang);
   return (
     <div className="hidden sticky md:flex top-0 z-10">
       <Card className="flex flex-row h-14 gap-4 w-full">
         <div className="container mx-auto px-8 lg:px-12 flex flex-row items-center">
           <Typography text="＜JasonBrutus /＞" type="h3" />
           <div className="ml-auto flex  flex-row items-center gap-5 p-4">
-            <Section name={header.home} section={"home"} inSection={false} />
-            <Section name={header.about} section={"about"} inSection={false} />
+            <Section name={menu.home} section={"home"} inSection={false} />
+            <Section name={menu.about} section={"about"} inSection={false} />
             <Section
-              name={header.projects}
+              name={menu.projects}
               section={"projects"}
               inSection={false}
             />
-            <Section name={header.work} section={"work"} inSection={false} />
+            <Section name={menu.work} section={"work"} inSection={false} />
             <Section
-              name={header.contact}
+              name={menu.contact}
               section={"contact"}
               inSection={false}
             />
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,5 +1,5 @@
 import { Locale } from "@/dictionaries/dictionaries";
-import { Header } from "@/Section/Header/ui/Header";
+import { Menu } from "@/Section/Menu/ui/Menu";
 import { Hero } from "@/Section/Hero/ui/Hero";
 
 export default async function Home({
@@ -10,7 +10,7 @@ export default async function Home({
   const { lang } = await params;
   return (
     <div className="relative">
-      <Header lang={lang} />
+      <Menu lang={lang} />
       <div className="container mx-auto px-8 lg:px-12">
         <Hero lang={lang} />
       </div>
