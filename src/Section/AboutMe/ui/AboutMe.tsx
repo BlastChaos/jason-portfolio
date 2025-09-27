8f1d9ae7b21fc9aab894891812c1d3116a142f64
BlastChaos <76956740+BlastChaos@users.noreply.github.com>
2025-09-26 22:52:00 -0400

    add coding competitor

--- src/Section/AboutMe/ui/AboutMe.tsx
+++ src/Section/AboutMe/ui/AboutMe.tsx
@@ -15,10 +15,10 @@ export const AboutMe: React.FC<Props> = async (props: Props) => {
       <div className="flex justify-center">
         <Header sectionName={aboutMe.sectionName} quote={aboutMe.quote} />
       </div>
-      <div className="flex flex-row items-center">
+      <div className="flex md:flex-row flex-col items-center">
         <Icons
           iconType={"meThinking"}
-          size={"5xl"}
+          size={"4xl"}
           className="rounded-full transition-transform duration-300 hover:scale-105"
         />
         <div className="flex flex-col gap-2">
@@ -29,20 +29,20 @@ export const AboutMe: React.FC<Props> = async (props: Props) => {
           <Typography type={"lg"} text={aboutMe.bioDescription1} />
           <Typography type={"lg"} text={aboutMe.bioDescription2} />
           <Typography type={"lg"} text={aboutMe.bioDescription3} />
-          <div className="md:grid md:grid-cols-3 flex flex-col gap-4 pt-2">
+          <div className="md:grid lg:grid-cols-3 md:grid-cols-2 flex flex-col gap-4 pt-2">
+            <InfoCard
+              icon={"idea"}
+              title={aboutMe.hobby3Tittle}
+              description={aboutMe.hobby3Description}
+            />
             <InfoCard
               icon={"controller"}
               title={aboutMe.hobby1Tittle}
               description={aboutMe.hobby1Description}
             />
             <InfoCard
-              icon={"controller"}
+              icon={"music"}
               title={aboutMe.hobby2Tittle}
-              description={aboutMe.hobby2Description}
-            />
-            <InfoCard
-              icon={"controller"}
-              title={aboutMe.hobby1Tittle}
               description={aboutMe.hobby2Description}
             />
           </div>
