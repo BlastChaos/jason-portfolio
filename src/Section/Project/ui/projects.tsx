f5d3e21d7a2bf3932a31567eb4f7ef7981bdb4c8
Jason Brutus <jasonbrutus@civalgo.com>
2025-10-30 21:02:43 -0400

    custom anime

--- src/Section/Project/ui/projects.tsx
+++ src/Section/Project/ui/projects.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { projectsInfo } from "../utilities/projectsInfo";
 import { Typography } from "@/components/ui/typography";
 import { TagBadge } from "../../shared/ui/tagBadge";
+import { CustomAnimation } from "@/Section/shared/ui/CustomAnimation";
 
 type Props = {
   lang: Locale;
@@ -12,44 +13,51 @@ export const Projects: React.FC<Props> = (props: Props) => {
   const projectsToSee = projectsInfo.slice(0, 3);
   return (
     <div className="md:grid lg:grid-cols-3 md:grid-cols-2 flex flex-col gap-4">
-      {projectsToSee.map((project) => (
-        <Card key={project.slug} className="pt-0 px-0 gap-2 group">
-          <div
-            style={{
-              backgroundColor: project.bgColor,
-            }}
-            className={`h-60 w-full rounded-t-xl relative overflow-hidden`}
-          >
-            <Image
-              src={project.icons[0]}
-              fill
-              alt={`${project.slug} icon`}
+      {projectsToSee.map((project, index) => (
+        <CustomAnimation
+          type="slideRightFadeIn"
+          delay={index * 0.2}
+          duration={1}
+          key={project.slug}
+        >
+          <Card className="pt-0 px-0 gap-2 group">
+            <div
               style={{
-                objectFit: "contain",
+                backgroundColor: project.bgColor,
               }}
-              className="object-contain transition-transform duration-300 group-hover:scale-105"
-            />
-          </div>
-
-          <div className="px-4">
-            <Typography
-              type={"h4"}
-              text={project.localizationInfo[props.lang].tittle}
-            />
-            <Typography
-              type={"sm"}
-              text={project.localizationInfo[props.lang].shortDescription}
-            />
-            <div className="flex flex-row flex-wrap gap-2">
-              {project.tags.slice(0, 5).map((tag) => (
-                <TagBadge tag={tag} key={`${project.slug}-${tag}`} />
-              ))}
-              {project.tags.length > 5 && (
-                <Card className="p-2">{`+${project.tags.length - 5}`}</Card>
-              )}
+              className={`h-60 w-full rounded-t-xl relative overflow-hidden`}
+            >
+              <Image
+                src={project.icons[0]}
+                fill
+                alt={`${project.slug} icon`}
+                style={{
+                  objectFit: "contain",
+                }}
+                className="object-contain transition-transform duration-300 group-hover:scale-105"
+              />
             </div>
-          </div>
-        </Card>
+
+            <div className="px-4">
+              <Typography
+                type={"h4"}
+                text={project.localizationInfo[props.lang].tittle}
+              />
+              <Typography
+                type={"sm"}
+                text={project.localizationInfo[props.lang].shortDescription}
+              />
+              <div className="flex flex-row flex-wrap gap-2">
+                {project.tags.slice(0, 5).map((tag) => (
+                  <TagBadge tag={tag} key={`${project.slug}-${tag}`} />
+                ))}
+                {project.tags.length > 5 && (
+                  <Card className="p-2">{`+${project.tags.length - 5}`}</Card>
+                )}
+              </div>
+            </div>
+          </Card>
+        </CustomAnimation>
       ))}
     </div>
   );
