8d3783b43a225c6dbf98c79e23851269e9e23707
BlastChaos <76956740+BlastChaos@users.noreply.github.com>
2025-11-17 22:43:39 -0500

    skill wip

--- src/Section/Skill/utilities/skillInfos.ts
+++ src/Section/Skill/utilities/skillInfos.ts
@@ -1,14 +1,11 @@
-type Skill = {
-  name: string;
-  icon: string;
-};
+// type Skill = {
+//   name: string;
+//   icon: string;
+// };
 
-type Section = {
-  name: string;
-  icon: string;
-};
+// type Section = {
+//   name: string;
+//   icon: string;
+// };
 
-
-export const skillInfos = [{
-    
-}]
+export const skillInfos = [{}];
